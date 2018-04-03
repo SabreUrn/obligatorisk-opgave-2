f02891d4028a403fe265e336b03ccdc11ef94408
Kasp5953 <kasp5953@edu.easj.dk>
2018-04-03 09:34:37 +0200

    opg 1 complete

---
+++ BoxVolumeCalculator/BoxVolumeCalculator/IService1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BoxVolumeCalculator {
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+	[ServiceContract]
+	public interface IService1 {
+
+		/*
+		[OperationContract]
+		string GetData(int value);
+
+		[OperationContract]
+		CompositeType GetDataUsingDataContract(CompositeType composite);
+		*/
+
+		// TODO: Add your service operations here
+		[OperationContract]
+		double GetVolume(double length, double width, double height);
+
+		[OperationContract]
+		double GetSide(double volume, double side1, double side2);
+	}
+
+
+	// Use a data contract as illustrated in the sample below to add composite types to service operations.
+	[DataContract]
+	public class CompositeType {
+		bool boolValue = true;
+		string stringValue = "Hello ";
+
+		[DataMember]
+		public bool BoolValue {
+			get { return boolValue; }
+			set { boolValue = value; }
+		}
+
+		[DataMember]
+		public string StringValue {
+			get { return stringValue; }
+			set { stringValue = value; }
+		}
+	}
+}
---
+++ BoxVolumeCalculator/BoxVolumeCalculatorTests/Service1Tests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoxVolumeCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxVolumeCalculator.Tests {
+	[TestClass()]
+	public class Service1Tests {
+		[TestMethod()]
+		public void GetVolumeTest_NormalNumbers() {
+			//arrange
+			Service1 service = new Service1();
+			double expected = 100.0;
+
+			//act
+			double actual = service.GetVolume(5, 5, 4);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void GetVolumeTest_OneZero() {
+			//arrange
+			Service1 service = new Service1();
+			double expected = 0;
+
+			//act
+			double actual = service.GetVolume(5, 5, 0);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void GetSideTest_NormalNumbers() {
+			//arrange
+			Service1 service = new Service1();
+			double expected = 3.0;
+
+			//act
+			double actual = service.GetSide(27, 3, 3);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void GetSideTest_VolumeZero() {
+			//arrange
+			Service1 service = new Service1();
+			double expected = 0.0;
+
+			//act
+			double actual = service.GetSide(0, 3, 3);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
